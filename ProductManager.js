04d9b3347513a8c50cf00aa190214a0d9be56b72
matirivero <matiezerivero@hotmail.com>
2024-03-19 20:13:18 -0300

    segundo desafio

--- ProductManager.js
+++ ProductManager.js
@@ -1,12 +1,37 @@
 
+const fs = require ("fs");
+
 class productManager{
     products;
+    path;
 
     static idIncrementable= 0;
 
 
     constructor(){
-        this.products = []
+        this.products = this.leerArchivo();
+        this.path = "./archivo/productos.json"
+    }
+
+    leerArchivo(){
+        try {
+            if(fs.existsSync(this.path))
+                return JSON.parse(fs.readFileSync(this.path,"utf-8"));
+            return [];
+            
+        } catch (error) {
+            console.log("No se puede leer el archivo")
+        }
+    }
+
+    actualizarArchivos(){
+        try {
+            fs.writeFileSync(this.path, JSON.stringify(this.products))
+            
+        } catch (error) {
+            console.log("error al actualizar archivo")
+            
+        }
     }
 
     addProduct(title,description,price,thumbnail,code,stock){
@@ -29,6 +54,7 @@ class productManager{
            };
 
            this.products.push(productoNuevo);
+           this.actualizarArchivos();
 
            return "Producto agregado exitosamente"
 
@@ -45,6 +71,27 @@ class productManager{
            return elemento
         else
            return `Not Found ${id}` 
+
+    }
+
+    updateProduct(id , propiedades){
+        const borrar = this.products. findborrar(p=> p.id === id);
+        if(borrar >= 0){
+            const {id, ...rest} = propiedades;
+            this.products[borrar] = {...this.products[borrar],...rest}
+            this.actualizarArchivos
+            console.log("producto actualizado")
+        }
+
+    }
+
+    deleteProduct(id){
+        const borrar = this.products.findborrar(p=> p.id === id);
+        if(borrar >= 0){
+            this.products = this.products.filter(p=> p.id !== id);
+            this.actualizarArchivos();
+        }
+        return "el id no existe"
 
     }
 
--- prueba.js
+++ prueba.js
@@ -3,5 +3,6 @@ const productManager = require("./ProductManager");
 
 const elemento = new productManager();
 
-console.log(elemento.getProducts());
-console.log(elemento.addProduct());
+console.log(elemento.addProduct("Mesa","mesa para living",10000,"https://img1.com","mt1",4));
+console.log(elemento.addProduct("banco","banco de jardin",20000,"https://img2.com","mt2",2));
+console.log(elemento.addProduct("Silla","silla comedor",4500,"https://img3.com","mt3",8));
